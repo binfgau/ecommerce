169268891d333dd644a238c1ac93d73adc06aa3a
Gấu <nguyenvanbinh13112000@outlook.com>
2022-05-07 22:28:49 +0700

    done layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { AiFillInstagram, AiFillFacebook } from 'react-icons/ai';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='footer-container'>
+      <p>2022 Binfgau Headphones All right reservers</p>
+      <p className='icons'>
+        <a href='https://www.instagram.com/im_binh/'>
+          <AiFillInstagram />
+        </a>
+        <a href='https://www.facebook.com/binfgau'>
+          <AiFillFacebook />
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
